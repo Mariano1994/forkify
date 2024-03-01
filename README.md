392ea5a64215b6ad4908200083db75c1ab0edf28
Mariano <marianocapiliku@gmail.com>
2024-03-01 01:16:26 +0100

    API Request to get an expecific recipe Using the fetch API to make a request to  the recipes API and get all the information about the recipe

--- README.md
+++ README.md
@@ -1,16 +1,20 @@
 # Forkify Application
+
 This a simple receipes application built using javascript langues, where the user can search for any recipe.
 The user can bookmark the recipe and once any recipe is bookmarked it's automatically add into the user account
 The user can also create ther own recipe.
 
 #### Used Technologies
-* JavaScript
-* Html
-* Sass for styling
 
-### Features 
-* Search for recipes (And the recipes is loaded from an external api)
-* List all the recipes, using pagination funcionality to separete the search results
-* Bookmark recipes, and once the recipe is book marked it's automatically add to the user account
-* Pagination funcionality
-* Allow the user to create its own recipe
+- Parcel
+- JavaScript
+- Html
+- Sass for styling
+
+### Features
+
+- Search for recipes (And the recipes is loaded from an external api)
+- List all the recipes, using pagination funcionality to separete the search results
+- Bookmark recipes, and once the recipe is book marked it's automatically add to the user account
+- Pagination funcionality
+- Allow the user to create its own recipe
--- dist/index.62406edb.js
+++ dist/index.62406edb.js
@@ -5,7 +5,32 @@ const timeout = function(s) {
             reject(new Error(`Request took too long! Timeout after ${s} second`));
         }, s * 1000);
     });
-}; // https://forkify-api.herokuapp.com/v2
- ///////////////////////////////////////
+};
+// https://forkify-api.herokuapp.com/v2
+///////////////////////////////////////
+// Function to fecthing Data from an external API
+const ShowRecipe = async function() {
+    try {
+        const response = await fetch("https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886");
+        const data = await response.json();
+        if (!response.ok) throw new Error(`${data.message} (${response.status})`);
+        let { recipe } = data.data;
+        // Based on the  fetching result we create a new recipe object with only need information
+        recipe = {
+            id: recipe.id,
+            title: recipe.title,
+            publisher: recipe.publisher,
+            sourceUrl: recipe.source_url,
+            image: recipe.image_url,
+            servings: recipe.servings,
+            cookingTime: recipe.cooking_time,
+            ingredients: recipe.ingredients
+        };
+        console.log(recipe);
+    } catch (err) {
+        alert(err.message);
+    }
+};
+ShowRecipe();
 
 //# sourceMappingURL=index.62406edb.js.map
